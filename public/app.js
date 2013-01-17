fffcc6920248623d3f699c250fa2eb645168a187
Richard <richard.anaya@gmail.com>
2013-01-16 22:04:57 -0600

    basic chat

--- public/app.js
+++ public/app.js
@@ -4,6 +4,8 @@ require.config({
     }
 })
 requirejs(['text!templates/front_page.html'], function(tmplHelloWorld){
+    var currentUser = Math.random()
+
     $(document.body).append(tmplHelloWorld);
     var resized = function(){
         $(".chat-sidebar").css("height",($(window).height()-150)+"px");
@@ -11,4 +13,37 @@ requirejs(['text!templates/front_page.html'], function(tmplHelloWorld){
     }
     $(window).resize(resized);
     resized()
+
+    var addMessage = function(m){
+        $(".chat-area").append(m+"<br>");
+    }
+
+    // LISTEN
+    PUBNUB.subscribe({
+        channel  : "hello_world",
+        callback : function(data){
+            if(currentUser != data.user){
+                addMessage(data.message);
+            }
+        }
+    })
+
+    var sendMessage = function(){
+        addMessage($('.chat-input').val());
+        PUBNUB.publish({
+            channel : "hello_world",
+            message : {user:currentUser,message:$('.chat-input').val()}
+        })
+        $('.chat-input').val("")
+    }
+
+    // SEND
+    $('.chat-send').click(function(){
+        sendMessage();
+    })
+    $('.chat-input').keydown(function(e){
+        if(e.keyCode == 13){
+            sendMessage();
+        }
+    })
 });
