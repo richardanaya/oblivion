cafdb3ae69a3577c136a5b57ef5552ccfa4ec5b0
Richard <richard.anaya@gmail.com>
2013-01-16 21:16:11 -0600

    creating initial design

--- public/app.js
+++ public/app.js
@@ -3,6 +3,12 @@ require.config({
         'text':'libs/requirejs/require-text'
     }
 })
-requirejs(['text!templates/helloworld.html'], function(tmplHelloWorld){
+requirejs(['text!templates/front_page.html'], function(tmplHelloWorld){
     $(document.body).append(tmplHelloWorld);
+    var resized = function(){
+        $(".chat-sidebar").css("height",($(window).height()-150)+"px");
+        $(".chat-area").css("height",($(window).height()-150)+"px");
+    }
+    $(window).resize(resized);
+    resized()
 });
